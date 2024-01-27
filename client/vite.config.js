626d0b59dd0ab0632816b9176ce315bc63443f8b
vairavan-ven <vairavan.ven@gmail.com>
2024-01-27 16:43:37 +1100

    added graph ql server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+    server: {
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
+      },
     }
   }
 })
