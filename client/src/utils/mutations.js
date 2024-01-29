4e1dfe3a2d4d3086be07de79e1bb492d699ab7fe
vairavan-ven <vairavan.ven@gmail.com>
2024-01-29 18:45:11 +1100

    made mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          _id
+          authors
+          description
+          title
+          bookId
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          _id
+          authors
+          description
+          title
+          bookId
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+    }
+  }
+`;
